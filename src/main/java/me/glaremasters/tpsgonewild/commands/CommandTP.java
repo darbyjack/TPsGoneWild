a77e086f10a0829506ed7b8637601175902930b2
darbyjack <admin@glaremasters.me>
2020-06-07 18:09:01 -0500

    Added permission node to tp command

--- src/main/java/me/glaremasters/tpsgonewild/commands/CommandTP.java
+++ src/main/java/me/glaremasters/tpsgonewild/commands/CommandTP.java
@@ -2,6 +2,7 @@ package me.glaremasters.tpsgonewild.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Dependency;
 import io.papermc.lib.PaperLib;
@@ -14,6 +15,7 @@ public class CommandTP extends BaseCommand {
     @Dependency private Tpsgonewild tpsgonewild;
 
     @Default
+    @CommandPermission("tpgw.tp")
     public void execute(Player player) {
         final Location location = tpsgonewild.getLocationUtil().getRandomLocationInsideWorldBorder(player.getWorld());
         PaperLib.teleportAsync(player, player.getWorld().getHighestBlockAt(location).getLocation());
