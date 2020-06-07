1122125007781b84b88cd485d2237ad55dd9c74b
darbyjack <admin@glaremasters.me>
2020-06-07 17:38:46 -0500

    Initial Commit

---
+++ src/main/java/me/glaremasters/tpsgonewild/Tpsgonewild.java
@@ -0,0 +1,17 @@
+package me.glaremasters.tpsgonewild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tpsgonewild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
